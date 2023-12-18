caa3d2b12a83e4f0042a7323620af3240d5ac35b
jin3811 <hjwoo1028@naver.com>
2023-12-18 15:06:08 +0900

    layout 수정

--- src/WatingRoomWindow/WaitingRoomPanel.java
+++ src/WatingRoomWindow/WaitingRoomPanel.java
@@ -16,38 +16,38 @@ public class WaitingRoomPanel extends JPanel {
     	this.nickname = nickname;
     	context.setSize(1000,800);
 
-    	setLayout(null);
-    	
+    	setLayout(new BorderLayout());
+
     	JPanel panelTop = new JPanel();
-    	panelTop.setBounds(0, 0, getWidth(), 70);
-    	this.add(panelTop);
-    	panelTop.setLayout(null);
-    	
-    	JLabel waitRoomLabel = new JLabel("대기방");
-    	waitRoomLabel.setHorizontalAlignment(SwingConstants.CENTER);
-    	waitRoomLabel.setBounds(0, 0, 114, 70);
-    	panelTop.add(waitRoomLabel);
-    	
-    	JButton createRoomBtn = new JButton("방만들기");
-    	createRoomBtn.addActionListener(new ActionListener() {
+		panelTop.setSize(getWidth(), 100);
+    	panelTop.setLayout(new BorderLayout());
+
+		JLabel waitRoomLabel = new JLabel("대기방");
+		waitRoomLabel.setHorizontalAlignment(SwingConstants.CENTER);
+//    	waitRoomLabel.setBounds(0, 0, 114, 70);
+		panelTop.add(waitRoomLabel, BorderLayout.WEST);
+
+		JButton createRoomBtn = new JButton("방만들기");
+		createRoomBtn.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
     		}
     	});
-    	createRoomBtn.setBounds(711, 0, 114, 70);
-    	panelTop.add(createRoomBtn);
-    	
-    	
-    	// JPanel을 생성하고 추가
-        JPanel innerPanel = new JPanel();
-        innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
+
+//    	createRoomBtn.setBounds(711, 0, 114, 70);
+		createRoomBtn.setSize(114, 70);
+		panelTop.add(createRoomBtn, BorderLayout.EAST);
+
+		add(panelTop, BorderLayout.NORTH);
+
+//		 JPanel을 생성하고 추가
+		JPanel innerPanel = new JPanel();
+		innerPanel.setLayout(new BoxLayout(innerPanel, BoxLayout.Y_AXIS));
         JScrollPane scrollPane = new JScrollPane(innerPanel);
-        scrollPane.setBounds(0, 70, 825, 524);
-        this.add(scrollPane);
-    	
-        
+        scrollPane.setSize(825, 524);
+		add(scrollPane, BorderLayout.CENTER);
     }
 
-    
+
 //    private void addWaitingRoom(String roomTitle, JPanel panel) {
 //        JButton waitingRoomButton = new JButton(roomTitle);
 //        waitingRoomButton.addActionListener(new ActionListener() {
