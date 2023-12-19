b41ffb334c8f86b7ffa8187a61675d954833baae
sb-111 <117798778+sb-111@users.noreply.github.com>
2023-12-19 15:04:35 +0900

    좌표 설정

--- src/UI/GamePanel.java
+++ src/UI/GamePanel.java
@@ -41,6 +41,8 @@ public class GamePanel extends JPanel {
 	List<Point> turrets = new ArrayList<>();
 
 	List<Point> allPoints = new ArrayList<>();
+	List<Point> bluePath;
+	List<Point> redPath;
 	public GamePanel(RandomDefence context, String nickname, Socket socket) {
 		this.context = context;
 
@@ -56,6 +58,149 @@ public class GamePanel extends JPanel {
 				allPoints.add(p);
 			}
 		}
+		redPath = new ArrayList<Point>() {
+			{
+				// 윗 부분
+				add(allPoints.get(22));
+				add(allPoints.get(23));
+				add(allPoints.get(24));
+				add(allPoints.get(25));
+				add(allPoints.get(26));
+				add(allPoints.get(27));
+				add(allPoints.get(28));
+				add(allPoints.get(29));
+				add(allPoints.get(41));
+				add(allPoints.get(49));
+				add(allPoints.get(61));
+				add(allPoints.get(69));
+				add(allPoints.get(81));
+				add(allPoints.get(89));
+				add(allPoints.get(101));
+				add(allPoints.get(109));
+				add(allPoints.get(121));
+				add(allPoints.get(129));
+				add(allPoints.get(141));
+				add(allPoints.get(149));
+				add(allPoints.get(161));
+				add(allPoints.get(169));
+				add(allPoints.get(181));
+				add(allPoints.get(182));
+				add(allPoints.get(183));
+				add(allPoints.get(184));
+				add(allPoints.get(185));
+				add(allPoints.get(186));
+				add(allPoints.get(187));
+				add(allPoints.get(188));
+				
+				// 아랫 부분
+				add(allPoints.get(201));
+				add(allPoints.get(202));
+				add(allPoints.get(203));
+				add(allPoints.get(204));
+				add(allPoints.get(205));
+				add(allPoints.get(206));
+				add(allPoints.get(207));
+				add(allPoints.get(208));
+
+				
+				add(allPoints.get(221));
+				add(allPoints.get(229));
+				add(allPoints.get(241));
+				add(allPoints.get(249));
+				add(allPoints.get(261));
+				add(allPoints.get(269));
+				add(allPoints.get(281));
+				add(allPoints.get(289));
+				add(allPoints.get(301));
+				add(allPoints.get(309));
+				add(allPoints.get(321));
+				add(allPoints.get(329));
+				add(allPoints.get(341));
+				add(allPoints.get(349));
+				
+				
+				add(allPoints.get(362));
+				add(allPoints.get(363));
+				add(allPoints.get(364));
+				add(allPoints.get(365));
+				add(allPoints.get(366));
+				add(allPoints.get(367));
+				add(allPoints.get(368));
+				add(allPoints.get(369));
+				
+			}
+		};
+		bluePath = new ArrayList<Point>() {
+			{
+				// 윗 부분
+				add(allPoints.get(30));
+				add(allPoints.get(31));
+				add(allPoints.get(32));
+				add(allPoints.get(33));
+				add(allPoints.get(34));
+				add(allPoints.get(35));
+				add(allPoints.get(36));
+				add(allPoints.get(37));
+				add(allPoints.get(50));
+				add(allPoints.get(58));
+				add(allPoints.get(70));
+				add(allPoints.get(78));
+				add(allPoints.get(90));
+				add(allPoints.get(98));
+				add(allPoints.get(110));
+				add(allPoints.get(118));
+				add(allPoints.get(130));
+				add(allPoints.get(138));
+				add(allPoints.get(150));
+				add(allPoints.get(158));
+				add(allPoints.get(170));
+				add(allPoints.get(178));
+				add(allPoints.get(191));
+				add(allPoints.get(192));
+				add(allPoints.get(193));
+				add(allPoints.get(194));
+				add(allPoints.get(195));
+				add(allPoints.get(196));
+				add(allPoints.get(197));
+				add(allPoints.get(198));
+				
+				// 아랫 부분
+				add(allPoints.get(211));
+				add(allPoints.get(212));
+				add(allPoints.get(213));
+				add(allPoints.get(214));
+				add(allPoints.get(215));
+				add(allPoints.get(216));
+				add(allPoints.get(217));
+				add(allPoints.get(218));
+
+				add(allPoints.get(230));
+				add(allPoints.get(238));
+				add(allPoints.get(250));
+				add(allPoints.get(258));
+				add(allPoints.get(270));
+				add(allPoints.get(278));
+				add(allPoints.get(290));
+				add(allPoints.get(298));
+				add(allPoints.get(310));
+				add(allPoints.get(318));
+				add(allPoints.get(330));
+				add(allPoints.get(338));
+				add(allPoints.get(350));
+				add(allPoints.get(358));
+				
+				
+				add(allPoints.get(370));
+				add(allPoints.get(371));
+				add(allPoints.get(372));
+				add(allPoints.get(373));
+				add(allPoints.get(374));
+				add(allPoints.get(375));
+				add(allPoints.get(376));
+				add(allPoints.get(377));
+				
+			}
+		};
 		System.out.println("좌표 리스트 크기: "+allPoints.size());
 		System.out.println("좌표 리스트 마지막: "+ allPoints.get(399));
 //		for(Point p: allPoints) {
@@ -126,11 +271,6 @@ public class GamePanel extends JPanel {
 		for (Point turret : turrets) {
 			g.drawImage(turret1Image, turret.x, turret.y, this);
 		}
-//		if (pathImage1 != null) {
-//			int pathWidth = pathImage1.getWidth(this);
-//			int pathHeight = pathImage1.getHeight(this);
-//			g.drawImage(pathImage1, 0, 0, pathWidth, pathHeight, this);
-//		}
 
 	}
 
@@ -139,35 +279,43 @@ public class GamePanel extends JPanel {
 			
 			int pathWidth = pathImage1.getWidth(this);
 			int pathHeight = pathImage1.getHeight(this);
-//			for(int i=2;i<=9;i++) {
+//			for(int i=22;i<=29;i++) {
 //				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
 //			}
-			for(int i=22;i<=29;i++) {
-				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
-			}
-//			for(int i=162;i<=168;i++) {
+//			for(int i=362;i<=369;i++) {
 //				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
 //			}
-			for(int i=180;i<=188;i++) {
-				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			for(int i=181;i<=188;i++) {
+//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			}
+//			for(int i=201;i<=208;i++) {
+//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			}
+//			for(int i=41;i<=161;i+=20) {
+//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			}
+//			for(int i=221;i<=341;i+=20) {
+//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			}
+//			for(int i=29;i<=169;i+=20) {
+//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			}
+//			for(int i=229;i<=349;i+=20) {
+//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
+//			}
+			for(Point p: redPath) {
+				g.drawImage(pathImage1, p.x, p.y, this);
 			}
-
-			
 		}
 	}
 	private void drawPath_blue(Graphics g) {
-//		if (pathImage2 != null) {
-//
-//			int pathWidth = pathImage2.getWidth(this);
-//			int pathHeight = pathImage2.getHeight(this);
-//			for(int i=2;i<=17;i++) {
-//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
-//			}
-//			for(int i=22;i<=37;i++) {
-//				g.drawImage(pathImage1, allPoints.get(i).x, allPoints.get(i).y, this);
-//			}
-//			
-//		}
+		if (pathImage2 != null) {
+			int pathWidth = pathImage2.getWidth(this);
+			int pathHeight = pathImage2.getHeight(this);
+			for(Point p: bluePath) {
+				g.drawImage(pathImage2, p.x, p.y, this);
+			}
+		}
 		
 	}
 
@@ -185,7 +333,7 @@ public class GamePanel extends JPanel {
 			int redWidth = redTeamImage.getWidth(this);
 			int redHeight = redTeamImage.getHeight(this);
 			g.drawImage(redTeamImage, 0, 0, redWidth, redHeight, this);
-			g.drawImage(redTeamImage, 0, 450, redWidth, redHeight, this);
+//			g.drawImage(redTeamImage, 0, 450, redWidth, redHeight, this);
 			g.drawImage(redTeamImage, 0, 900, redWidth, redHeight, this);
 		}
 	}
@@ -195,7 +343,7 @@ public class GamePanel extends JPanel {
 			int blueWidth = blueTeamImage.getWidth(this);
 			int blueHeight = blueTeamImage.getHeight(this);
 			g.drawImage(blueTeamImage, 900, 0, blueWidth, blueHeight, this);
-			g.drawImage(blueTeamImage, 900, 450, blueWidth, blueHeight, this);
+//			g.drawImage(blueTeamImage, 900, 450, blueWidth, blueHeight, this);
 			g.drawImage(blueTeamImage, 900, 900, blueWidth, blueHeight, this);
 		}
 
