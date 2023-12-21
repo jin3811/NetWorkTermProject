e6735e52c095d05b90a6a5b5408c27129a3cf968
jin3811 <hjwoo1028@naver.com>
2023-12-21 17:44:57 +0900

    panel들이 가져야할 공통 속성

---
+++ src/UI/MultiRoomJPanel.java
@@ -0,0 +1,46 @@
+package UI;
+
+import javax.swing.*;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public abstract class MultiRoomJPanel extends JPanel {
+    protected Socket socket;
+    protected ObjectOutputStream objOs;
+    protected ObjectInputStream objIs;
+
+    protected String nickname;
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public ObjectOutputStream getObjOs() {
+        return objOs;
+    }
+
+    public void setObjOs(ObjectOutputStream objOs) {
+        this.objOs = objOs;
+    }
+
+    public ObjectInputStream getObjIs() {
+        return objIs;
+    }
+
+    public void setObjIs(ObjectInputStream objIs) {
+        this.objIs = objIs;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
