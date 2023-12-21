34a597c0695940048c0d03ffe6f9a705c2932755
jin3811 <hjwoo1028@naver.com>
2023-12-21 17:44:39 +0900

    메인 프레임에서 모든 패널이 공유하는 속성을 자신이 관리함

--- src/UI/RandomDefence.java
+++ src/UI/RandomDefence.java
@@ -5,10 +5,17 @@ import util.TransitionDisplayCommand;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 
 public class RandomDefence extends JFrame{
 
-    private LoginFormPanel loginFormPanel;
+    private JPanel rootPanel;
+    private Socket socket;
+    private ObjectInputStream objIs;
+    private ObjectOutputStream objOs;
+    private String nickname;
 
     public RandomDefence(String title) {
         super(title);
@@ -20,26 +27,64 @@ public class RandomDefence extends JFrame{
     }
 
     private void initWindow() {
-        loginFormPanel = new LoginFormPanel(this);
+        rootPanel = new LoginFormPanel(this);
 
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.insets = new Insets(50, 50, 50, 50); // 내부 여백 설정
-        add(loginFormPanel, gbc);
+        add(rootPanel, gbc);
     }
 
-    public void transition(JPanel displayTarget, TransitionDisplayCommand displayCommand) {
+    public void transition(MultiRoomJPanel displayTarget, TransitionDisplayCommand displayCommand) {
+        if (displayCommand != null) displayCommand.execute();
+
+        rootPanel = displayTarget;
+        displayTarget.setSocket(socket);
+        displayTarget.setObjOs(objOs);
+        displayTarget.setObjIs(objIs);
+
         getContentPane().removeAll();
         getContentPane().add(displayTarget);
         setContentPane(displayTarget);
         getContentPane().revalidate();
         getContentPane().repaint();
-        if (displayCommand != null) displayCommand.execute();
     }
 
-    public void transition(JPanel displayTarget) {
+    public void transition(MultiRoomJPanel displayTarget) {
         transition(displayTarget, null);
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public ObjectInputStream getObjIs() {
+        return objIs;
+    }
+
+    public void setObjIs(ObjectInputStream objIs) {
+        this.objIs = objIs;
+    }
+
+    public ObjectOutputStream getObjOs() {
+        return objOs;
+    }
+
+    public void setObjOs(ObjectOutputStream objOs) {
+        this.objOs = objOs;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 }
